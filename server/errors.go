8254e212371b965d440fdb39ce714e9856eb41f7
akzi <382018309@qq.com>
2020-08-27 22:13:55 +0800

    clean all

--- server/errors.go
+++
@@ -1,9 +0,0 @@
-package server
-
-import "errors"
-
-var (
-	ErrNoFindStream       = errors.New("no find stream error")
-	ErrInvalidStreamEnd   = errors.New("invalid stream end error")
-	ErrInvalidStreamBegin = errors.New("invalid stream begin error")
-)
