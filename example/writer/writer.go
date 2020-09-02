ecf643904be651c98d5a2321394c8970c0fa5133
akzi <fu.niukey@gmail.com>
2020-09-02 11:15:56 +0800

    add example

---
+++ example/writer/writer.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"github.com/akzj/streamIO/client"
+	"time"
+)
+
+func main() {
+	var metaServer string
+	var data string
+	flag.StringVar(&metaServer, "ms", "127.0.0.1:5000", "--ms [ip:port]")
+	flag.StringVar(&data, "data", "hello"+time.Now().String(), "--data []")
+	flag.Parse()
+
+	ctx := context.Background()
+	msClient, err := client.NewMetaServiceClient(ctx, metaServer)
+	if err != nil {
+		panic(err)
+	}
+	client := client.NewClient(msClient)
+
+	streamID, err := client.GetOrCreateStream(ctx, "hello")
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Println(streamID)
+
+	session, err := client.NewStreamSession(ctx, 1, "hello")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	writer, err := session.NewWriter()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	n, err := writer.Write([]byte("hello world"))
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Println(n)
+}
