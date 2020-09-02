308b5bea5839f9a297ac304c935cb1d1f6ffbc3a
akzi <fu.niukey@gmail.com>
2020-09-02 18:04:02 +0800

    cli add test

--- cmd/cli/cli.go
+++ cmd/cli/cli.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"crypto/md5"
 	"fmt"
 	"github.com/akzj/streamIO/client"
 	"github.com/akzj/streamIO/meta-server/store"
@@ -12,6 +13,7 @@ import (
 	log "github.com/sirupsen/logrus"
 	"github.com/urfave/cli/v2"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -19,6 +21,7 @@ var ctx = context.Background()
 
 func init() {
 	log.SetOutput(os.Stderr)
+	log.SetReportCaller(true)
 }
 func addStreamServer(metaServer string, id int64, addr string) error {
 	client, err := client.NewMetaServiceClient(ctx, metaServer)
@@ -67,22 +70,117 @@ func listStreamServer(metaServer string) error {
 	return nil
 }
 
+func writeStream(metaServer string, streamName string, data string, count int64) error {
+	fmt.Println(metaServer, streamName, data, count)
+	msClient, err := client.NewMetaServiceClient(ctx, metaServer)
+	if err != nil {
+		panic(err)
+	}
+	client := client.NewClient(msClient)
+	defer func() {
+		client.Close()
+	}()
+
+	if _, err := client.GetOrCreateStream(ctx, streamName); err != nil {
+		log.Error(err.Error())
+		return err
+	}
+
+	session, err := client.NewStreamSession(ctx, 1, streamName)
+	if err != nil {
+		log.Error(err)
+		return nil
+	}
+
+	writer, err := session.NewWriter()
+	if err != nil {
+		log.Error(err)
+		return nil
+	}
+	defer writer.Close()
+	var size int
+	hash := md5.New()
+	for count := count; count > 0; count-- {
+		buffer := string(data) + strconv.Itoa(int(count)) + "\n"
+		n, err := writer.Write([]byte(buffer))
+		if err != nil {
+			panic(err.Error())
+		}
+		if n != len(buffer) {
+			panic("write error")
+		}
+		size += n
+		hash.Write([]byte(buffer))
+	}
+	fmt.Printf("write stream [%s] count [%d]  size [%d] md5[%x]\n", streamName, count, size, hash.Sum(nil))
+	return nil
+}
+
+func readStream(metaServer string, streamName string, size int64) error {
+	msClient, err := client.NewMetaServiceClient(ctx, metaServer)
+	if err != nil {
+		panic(err)
+	}
+	client := client.NewClient(msClient)
+	defer func() {
+		client.Close()
+	}()
+
+	if _, err := client.GetOrCreateStream(ctx, streamName); err != nil {
+		log.Error(err.Error())
+		return err
+	}
+
+	session, err := client.NewStreamSession(ctx, 1, streamName)
+	if err != nil {
+		log.Error(err)
+		return nil
+	}
+
+	reader, err := session.NewReader()
+	if err != nil {
+		log.Error(err)
+		return nil
+	}
+	hash := md5.New()
+
+	for toRead := size; toRead > 0; {
+		var data []byte
+		if toRead > 1024 {
+			data = make([]byte, 1024)
+		} else {
+			data = make([]byte, toRead)
+		}
+		n, err := reader.Read(data)
+		if err != nil {
+			log.Error(err)
+			return nil
+		}
+		data = data[:n]
+		hash.Write(data)
+		//fmt.Print(string(data))
+		toRead -= int64(n)
+	}
+	fmt.Printf("read stream [%s] size [%d] md5[%x]\n", streamName, size, hash.Sum(nil))
+	return nil
+}
+
 func main() {
 	app := cli.App{
 		EnableBashCompletion: true,
 		Flags: []cli.Flag{
 			&cli.StringFlag{
-				Name:     "ms",
-				Aliases:  []string{"m"},
-				Usage:    "meta-server-addr",
-				Value:    "127.0.0.1:5000",
+				Name:    "ms",
+				Aliases: []string{"m"},
+				Usage:   "meta-server-addr",
+				Value:   "127.0.0.1:5000",
 			},
 		},
 		Commands: []*cli.Command{
 			{
 				Name:    "stream-server",
 				Aliases: []string{"s"},
-				Usage:   "manager stream-server",
+				Usage:   "stream-server operator",
 				Subcommands: []*cli.Command{
 					{
 						Name: "list",
@@ -113,6 +211,57 @@ func main() {
 							id := c.Int64("id")
 							addr := c.String("addr")
 							return addStreamServer(metaServerAddr, id, addr)
+						},
+					},
+				},
+			},
+			{
+				Name:  "test",
+				Usage: "test tools",
+				Subcommands: []*cli.Command{
+					{
+						Name: "write",
+						Flags: []cli.Flag{
+							&cli.StringFlag{
+								Name:  "name",
+								Usage: "stream name",
+								Value: "test-stream-02",
+							},
+							&cli.Int64Flag{
+								Name:  "count",
+								Usage: "write count",
+								Value: 1024,
+							},
+							&cli.StringFlag{
+								Name:  "data",
+								Usage: "data to write",
+								Value: "hello world",
+							},
+						},
+						Action: func(c *cli.Context) error {
+							return writeStream(c.String("ms"),
+								c.String("name"),
+								c.String("data"),
+								c.Int64("count"))
+						},
+					},
+					{
+						Name: "read",
+						Flags: []cli.Flag{
+							&cli.StringFlag{
+								Name:     "name",
+								Usage:    "stream name",
+								Required: false,
+								Value:    "test-stream-02",
+							},
+							&cli.Int64Flag{
+								Name:     "size",
+								Usage:    "read size",
+								Required: false,
+							},
+						},
+						Action: func(c *cli.Context) error {
+							return readStream(c.String("ms"), c.String("name"), c.Int64("size"))
 						},
 					},
 				},
